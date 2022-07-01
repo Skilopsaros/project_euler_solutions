90933b78b4c15d96b7153a44d4ac811f465b4386
skilopsaros <skilopsaros@gmail.com>
2022-07-01 16:48:47 +0300

    renaming stuff

---
+++ problem_010/solution_1/p010_s1_main.py
@@ -0,0 +1,21 @@
+import math as maths
+import numpy as np
+
+def main(up_to):
+	primes_list = []
+	for i in range(2, up_to):
+		if is_next_prime(i, primes_list):
+			primes_list.append(i)
+	return(np.sum(np.array(primes_list)))
+
+def is_next_prime(n, list):
+	for i in list:
+		if (maths.floor(pow(n,0.5))+1) < i:
+			return(True)
+		if n%i==0:
+			return(False)
+	return(True)
+
+
+if __name__ == "__main__":
+	print(main(2000000))
